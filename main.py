a9a0bb7a73844c22e0cc9fc8bb4b7f5227b37489
Deovyonne Patterson <85905465+DeoPatt@users.noreply.github.com>
2021-07-04 05:56:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+#this is my first time coding python
+print("Hello world!")
+num1 = 5
+num2 = 2
+mesasge = "new message"
+sum = num1 + num2
+print(sum)
+diff = num1 - num2
+print(diff)
+prod = num1 * num2
+print(prod)
+quotient = num1/num2
+print(quotient)
+remainder = num1%num2
+print(remainder)
+age = input("enter your age")
+if (int(age) < 21):
+    print("you cannot vote yet")
+else:
+     print("Please vote!")
+
+
+km = input("Enter how many kilometers you drove")
+miles = float(km) * 0.62
+print(miles)
+
+
+
+ 
